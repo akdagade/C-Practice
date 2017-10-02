4d495625e14b999f3bf5a05e11496935039a2d9f
Akshay Dagade <akdagade@gmail.com>
2017-10-02 10:23:28 +0530

    Rev linked list

--- Data Strcture/01/linked_list.c
+++ Data Strcture/01/linked_list.c
@@ -14,12 +14,22 @@ void printList(struct Node* head);
 struct Node*  insertStart(struct Node* head, int num);
 struct Node* insertAt(struct Node* head, int num, int pos);
 struct Node* deleteAt(struct Node* head, int pos);
+struct Node* revList(struct Node* head);
 
 int main(){
 
     struct Node* head = NULL;
 
     int n,ele,p;
+    /*
+    head = insertStart(head,1);
+    head = insertStart(head,2);
+    head = insertStart(head,3);
+    head = insertStart(head,4);
+    head = insertStart(head,5);
+    head = insertStart(head,6);
+    printList(head);*/
+
     printf("Enter the number of elements : ");
     scanf("%d",&n);
 
@@ -32,14 +42,19 @@ int main(){
         head = insertAt(head,ele,p);
         printList(head);
     }
+
     printf("\n~~~~~Delete~~~~~\n");
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < 2; ++i) {
 
         printf("Enter the position to delete : ");
         scanf("%d",&p);
         head = deleteAt(head,p);
         printList(head);
     }
+
+    printf("\n~~~~~Reverse~~~~~\n");
+    head = revList(head);
+    printList(head);
 
     return 0;
 }//main
@@ -51,12 +66,13 @@ void printList(struct Node* head){
         printf("\n No elements in the List!!\n");
     }else {
 
-        while (head->next != NULL) {
+        while (head != NULL) {
             printf("%d\t", head->data);
             head = head->next;
         }
 
-        printf("%d\n", head->data);
+        printf("\n");
+
     }
 }//printList
 
@@ -79,18 +95,18 @@ struct Node* insertAt(struct Node* head, int num, int pos){
         printf("\nHead is not initialized, insert at position 1!!\n");
         return head;
     }
+
     struct Node* hd=head;
+    if(pos==1){
+        return insertStart(hd,num);
+    }
+
     struct Node* tmp = (struct Node*) malloc(sizeof(struct Node));
     tmp->data=num;
     tmp->next=NULL;
     int counter=1;
 
-
-    if(pos==1){
-        return insertStart(hd,num);
-    }
-
-    while (head->next!=NULL){
+    while (head!=NULL){
             if(pos-1==counter){
                 tmp->next= head->next;
                 head->next = tmp;
@@ -100,11 +116,7 @@ struct Node* insertAt(struct Node* head, int num, int pos){
             counter++;
         }
 
-    if(counter==pos-1){
-        head->next=tmp;
-    } else {
-        printf("\nYou have entered invalid no of elements!!\n");
-    }
+    printf("\nYou have entered invalid no of elements!!\n");
     return hd;
 }//insertAt
 
@@ -137,14 +149,33 @@ struct Node* deleteAt(struct Node* head, int pos){
         counter++;
     }
 
-    if(counter==pos){
-        free(prv->next);
-        prv->next=NULL;
-
-    } else {
-        printf("\nYou have entered invalid no of elements!!\n");
-    }
+    printf("\nYou have entered invalid no of elements!!\n");
     return hd;
 
 }//deleteAt
 
+struct Node* revList(struct Node* head){
+
+    struct Node* tmp=NULL;
+    struct Node* prv=NULL;
+    int counter=1;
+
+    if(counter=1 && head->next==NULL){
+        return head;
+    }
+
+    while (head!=NULL){
+
+        tmp= head->next;
+        if(counter==1){
+            head->next=NULL;
+        }
+        else{
+            head->next=prv;
+        }
+        prv = head;
+        head = tmp;
+    }
+
+    return prv;
+}//revList
